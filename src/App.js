63b4fdf35420706cfad0cd772e3a280c8d3b7aed
Nikolaus Matten <nfmatten@gmail.com>
2022-10-25 12:16:50 -0600

    Created NavBar component + Created table to be displayed with DisplayMusic component + Added useState and useEffect to App component to get Songs data and using Asnyc function to get songs from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import NavBar from "./Components/NavBar/NavBar";
+import React, { useEffect ,useState } from 'react';
+import DisplayMusic from "./Components/DisplayMusic/DisplayMusic";
+import axios from "axios";
+
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data);
+  }
+
   return (
     <div>
+      <NavBar/>
+      <DisplayMusic parentSongs={songs}/>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+
+function NavBar(){
+    return (
+        <nav className="margin">
+            <ul>
+                <li>
+                    <a className="active styling" href="https://github.com/NFMatten/Project_MusicLibrary_Frontend.git">Music Library</a> 
+                </li>               
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
